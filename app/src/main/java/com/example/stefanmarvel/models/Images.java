6090f47d6e3ddfd73cd80ba62cf33f375e46872f
Steff May <stefan.may@bizarreware.co.uk>
2017-07-06 14:58:12 +0100

    added quick retrieval method that checks if object is null or any of the parts needed for retrieving the image are null

--- app/src/main/java/com/example/stefanmarvel/models/Images.java
+++ app/src/main/java/com/example/stefanmarvel/models/Images.java
@@ -7,4 +7,16 @@ package com.example.stefanmarvel.models;
 public class Images {
     public String path;
     public String extension;
+
+    public static String getPathAndExt(Images images) {
+        if(images != null) {
+            if(images.path != null && images.extension != null)
+                return images.getPathAndExt();
+        }
+        return null;
+    }
+
+    public String getPathAndExt() {
+        return path + "." + extension;
+    }
 }
